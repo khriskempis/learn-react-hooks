3bd5a38761e02f131291eb1cb96edd0c9ffb02ab
Khris Kempis <khriskempis@gmail.com>
2019-12-14 15:31:36 -0800

    add click button using hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
-import "./App.css";
+import { hot } from "react-hot-loader";
+import "./css/main.css";
+
+import CountButton from "./input";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> Hello, World! </h1>
+        <h1>Hello, World! </h1>
+        <p>some more changes</p>
+        <p>even more chnages</p>
+        <CountButton />
       </div>
     );
   }
 }
 
-export default App;
+export default hot(module)(App);
